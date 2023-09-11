83cf8e9670251d7ccc9a4eeb94c136ec84a6de38
Luan_Marcal <luan.marcal@dell.com>
2023-09-11 15:24:57 -0300

    first commit

---
+++ src/main/java/br/com/diretoaoponto/demogRPC/DemoGRpcApplication.java
@@ -0,0 +1,13 @@
+package br.com.diretoaoponto.demogRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGRpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diretoaoponto/demogRPC/DemoGRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diretoaoponto.demogRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
